ce3d8550dac67bcc58d72d9772f368869908b08d
NishchayDixit <dixitnishchay4@gmail.com>
2023-11-28 19:39:52 +0530

    added AOS in site

--- src/components/Sections.js
+++ src/components/Sections.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Element } from 'react-scroll';
 import './Sections.css'
 import HeroSection from './hero_section/HeroSection';
 import AboutSection from './about_section/AboutSection';
 import ProjectSection from './project_section/ProjectSection';
 import SkillsSection from './skills_section/SkillsSection';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const Sections = () => {
+
+    useEffect(() => { 
+        Aos.init({
+            once: true,
+        });
+    }, [])
+
     return (
         <>
             <Element id='home' name='home' className='home'>
                 <HeroSection />
             </Element>
-            <Element id='about' name='about' className='portfolio_content'>
+            <Element id='about' name='about' className='portfolio_content' data-aos='fade-up'>
                 <AboutSection />
             </Element>
-            <Element id='project' name='project' className='portfolio_content exception'>
+            <Element id='project' name='project' className='portfolio_content exception' data-aos='fade-up'>
                 <ProjectSection />
             </Element>
-            <Element id='skill' name='skill' className='portfolio_content'>
+            <Element id='skill' name='skill' className='portfolio_content' data-aos='fade-up'>
                 <SkillsSection />
             </Element>
         </>
--- src/components/project_section/ProjectSection.js
+++ src/components/project_section/ProjectSection.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ProjectSection.css';
 import SectionHeading from '../../widgets/section_heading/SectionHeading';
 import ProjectContainer from '../../widgets/project_container/ProjectContainer';
-import first_project from '../../assets/image/Project 2.svg';
+import first_project from '../../assets/image/Project 2.png';
 import second_project from '../../assets/image/Project 1.svg';
 import third_project from '../../assets/image/Project 4.svg';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 
 const ProjectSection = () => {
+
+    useEffect(() => { 
+        Aos.init({
+            once: true,
+        });
+    }, [])
+
     return (
         <>
             <SectionHeading secction_name='My Recent Work' />
             <div className='projects'>
-                <div className='container'>
+                <div className='container' data-aos='fade-right'>
                     <ProjectContainer projectImg={first_project} projectName='RMTS-BRTS' projectType='Mobile App (Flutter)' projectDescription='Rajkot city bus mobile app provides route details, timings, charges,
                         live tracking, simplifying public transit. Enhancing user experience with comprehensive features
                         for efficient navigation.'/>
                 </div>
-                <div className='container'>
+                <div className='container' data-aos='fade-left'>
                     <ProjectContainer projectImg={second_project} projectName='Finance App' projectType='Application Design (Figma, Adobe XD)' projectDescription='User-centric design for seamless financial management. Features
                         include budget tracking, expense analysis, and intuitive interfaces for efficient money
                         management.'/>
                 </div>
-                <div className='container'>
+                <div className='container' data-aos='fade-right'>
                     <ProjectContainer projectImg={third_project} projectName='eSpectra' projectType='Logo Design (Adobe Illustrator)' projectDescription='The logo symbolizes dynamic synergy, innovation, and diversity.
                         Vibrant colors embody our commitment to a dynamic, inclusive future, reflecting the essence of
                         our company.'/>
