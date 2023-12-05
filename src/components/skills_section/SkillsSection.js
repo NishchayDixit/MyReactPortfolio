3e29f2439b032870286bdb029a04b822c6b70939
NishchayDixit <dixitnishchay4@gmail.com>
2023-12-05 21:38:38 +0530

    changes in global css

--- src/components/skills_section/SkillsSection.js
+++ src/components/skills_section/SkillsSection.js
@@ -2,14 +2,10 @@ import React from 'react';
 import './SkillsSection.css';
 import SectionHeading from '../../widgets/section_heading/SectionHeading';
 import SkillContainer from '../../widgets/skill_container/SkillContainer';
-import web_dev from '../../assets/image/web_dev.png';
-import mobile_dev from '../../assets/image/mobile_dev.png';
-import ui_ux_design from '../../assets/image/ui_ux_design.png';
-import graphic_design from '../../assets/image/graphic_design.png';
 
 
 const SkillsSection = () => { 
-    const webDevelopmentTech = ['JavaScript', 'ReactJS', 'NodeJS'];
+    const webDevelopmentTech = ['HTML', 'CSS', 'JavaScript', 'ReactJS', 'NodeJS'];
     const mobileDevelopmentTech = ['Android', 'Flutter'];
     const uiuxdesign = ['Figma', 'Adobe XD'];
     const graphicdesign = ['Adobe Illustrator'];
@@ -20,12 +16,12 @@ const SkillsSection = () => {
             <div className='skills'>
                 <div className='skill-content'>
                     <div className='view'>
-                        <SkillContainer category='Web Development' image={web_dev} techNames={webDevelopmentTech}/>
-                        <SkillContainer category='App Development' image={mobile_dev} techNames={mobileDevelopmentTech}/>
+                        <SkillContainer category='Web Development' techNames={webDevelopmentTech}/>
+                        <SkillContainer category='App Development' techNames={mobileDevelopmentTech}/>
                     </div>
                     <div className='view'>
-                    <SkillContainer category='UI/UX Design' image={ui_ux_design} techNames={uiuxdesign}/>
-                        <SkillContainer category='Graphic Design' image={graphic_design} techNames={graphicdesign}/>
+                    <SkillContainer category='UI/UX Design' techNames={uiuxdesign}/>
+                        <SkillContainer category='Graphic Design' techNames={graphicdesign}/>
                     </div>
                 </div>
             </div>
--- src/widgets/skill_container/SkillContainer.js
+++ src/widgets/skill_container/SkillContainer.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import './SkillContainer.css';
 
-const SkillContainer = ({ category, image, techNames }) => {
+const SkillContainer = ({ category, techNames }) => {
     return (
         <>
             <div className='s-container'>
                 <div className='heading'>
-                    <img src={image} alt={`${category} Icon`} />
                     <h3>{category}</h3>
                 </div>
                 <div className='tech'>
                     {techNames.map((tech, index) => (
                         <React.Fragment key={index}>
                             <div className='tech-name'>{tech}</div>
-                            {index !== techNames.length - 1 && <hr />}
                         </React.Fragment>
                     ))}
                 </div>
